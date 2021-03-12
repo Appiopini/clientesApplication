63462a0945141ec43f926a24a483c14c0a41205d
Appiopini <appiopini@gmail.com>
2021-03-12 15:34:01 -0300

    lombok installed

---
+++ src/main/java/com/github/appiopini/clientes/model/entity/Cliente.java
@@ -0,0 +1,26 @@
+package com.github.appiopini.clientes.model.entity;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.*;
+import java.time.LocalDate;
+
+@Entity
+@Getter@Setter
+public class Cliente {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    @Column(nullable = false, length = 150)
+    private String name;
+
+    @Column(nullable = false, length = 11)
+    private String cpf;
+
+    @Column
+    private LocalDate dataCadastro;
+
+
+}
